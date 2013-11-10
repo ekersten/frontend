9eca18350fe8e9093ce44edb70ec5040a1405c4a
Eric Kersten <ekersten@gmail.com>
2013-11-10 20:40:19 -0300

    final clase 5
    backbone + nodejs (con PonyExpress)

---
+++ backbone/puls4/public/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	console.log('main.js loaded');
+
+	window.views.app = new Puls4.Views.App($('body'));
+
+	window.ponyExpress = new PonyExpress({
+		io: window.location.origin
+	});
+
+	window.ponyExpress.bind('connect', function(){
+		window.plugs.article = new PonyExpress.BackbonePlug({
+			collection: window.collections.articles
+		});
+	});
+
+	window.collections.articles = new Puls4.Collections.Articles();
+
+	window.collections.articles.on('add', function (model) {
+		// console.log('Se agregro un nuevo articulo', model.toJSON());
+		// Aqui agregaremos una vista para cada uno de nuestros articulos
+		var view = new Puls4.Views.Article({model:model});
+
+		view.render();
+		view.$el.prependTo('.posts');
+	});
+
+	window.collections.articles.fetch();
+});
